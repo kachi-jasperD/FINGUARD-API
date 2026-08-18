0ef8a3668e59c966e01bb28d0c04047a12d820ef
Onyekachi Jasper-Duruzor <ojasperduruzor@yahoo.com>
2026-08-18 14:30:18 +0100

    Implemented openAI

---
+++ src/config/openAI.js
@@ -0,0 +1,13 @@
+import OpenAI from "openai";
+const client = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const response = await client.responses.create({
+  model: "gpt-5.6",
+  reasoning: { effort: "low" },
+  instructions: "Talk like a pirate.",
+  input: "Are semicolons optional in JavaScript?",
+});
+
+console.log(response.output_text);
